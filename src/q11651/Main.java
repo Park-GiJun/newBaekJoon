42ad3b3225dc2fafb28b1d5af6288cd689a18a07
Park-GiJun <144126780+Park-GiJun@users.noreply.github.com>
2023-10-16 17:09:32 +0900

    11651

---
+++ src/q11651/Main.java
@@ -0,0 +1,42 @@
+package q11651;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+
+		int[][] arr = new int[n][2];
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 2; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		Arrays.sort(arr, (o1, o2) -> {
+			if (o1[1] == o2[1]) {
+				return Integer.compare(o1[0], o2[0]);
+			}
+			return Integer.compare(o1[1], o2[1]);
+		});
+
+		for (int i = 0; i < n; i++) {
+			bw.write(arr[i][0] + " " + arr[i][1] + "\n");
+		}
+
+		bw.flush();
+		bw.close();
+	}
+}
