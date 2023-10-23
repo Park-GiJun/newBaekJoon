94994a62039f807be647e3fc750a3fd73ace7f7a
Park-GiJun <144126780+Park-GiJun@users.noreply.github.com>
2023-10-23 14:39:52 +0900

    1764

--- src/q1764/Main.java
+++ src/q1764/Main.java
@@ -1,37 +1,48 @@
 package q1764;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
         Map<String, Integer> listen = new HashMap<>();
         for (int i = 0; i < n; i++) {
             String inp = br.readLine();
-            listen.put(inp, i+1);
+            listen.put(inp, i + 1);
         }
+
         Map<String, Integer> view = new HashMap<>();
-        for (int i = 0 ; i < m ; i++){
+        for (int i = 0; i < m; i++) {
             String inp = br.readLine();
-            view.put(inp, i+1);
-
+            view.put(inp, i + 1);
         }
 
-        List<String> list = new ArrayList<>();
+        List<String> listA = new ArrayList<>();
 
+        for (Map.Entry<String, Integer> listenEntry : listen.entrySet()) {
+            String listB = listenEntry.getKey();
+            if (view.containsKey(listB)) {
+                listA.add(listB);
+            }
+        }
+        List<String> sortedListA = listA.stream()
+                .sorted()
+                .collect(Collectors.toList());
+        bw.write(String.valueOf(sortedListA.size()) + "\n");
+        for (String item : sortedListA) {
+            bw.write(item + "\n");
+        }
 
+        br.close();
+        bw.close();
     }
 }
